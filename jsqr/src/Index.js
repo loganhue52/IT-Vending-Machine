d45abb1dcf312a3be5e50165e55e01c27ced12fb
kryock-com <kevin.yockey@evsck12.com>
2021-11-30 12:39:50 -0600

    update

--- jsqr/src/Index.js
+++ jsqr/src/Index.js
@@ -1,25 +1,15 @@
-
-//resoure - https://www.geeksforgeeks.org/generate-a-qr-code-in-node-js/
-
 // Require the package
 const QRCode = require('qrcode')
 
 // Creating the data
-
-
-let website = "https://docs.google.com/forms/d/e/1FAIpQLSfYzGHDtafCUIET58TOIOT8l7bnKdnWnDhae5rmWol6WFZAnQ/viewform?usp=pp_url&entry.1252295625=computer+networking"
-//let parameters = "?tag=kevin&desc=17&loc=Classroom"
+let website = "https://docs.google.com/forms/d/e/1FAIpQLSfYzGHDtafCUIET58TOIOT8l7bnKdnWnDhae5rmWol6WFZAnQ/viewform?usp=pp_url&entry.1252295625=cs&entry.1878835824=tuesday"
 let data = website
 console.log(data)
-//let data = "fire Base website"
 
-// Converting the data into String format
-//let stringdata = JSON.stringify(data)
 
 // Print the QR code to terminal
 QRCode.toString(data,{type:'terminal'},
 					function (err, QRcode) {
-
 	if(err) return console.log("error occurred")
 
 	// Printing the generated code
